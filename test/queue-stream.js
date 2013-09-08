41e932bb00d73d1b7c97252b8e91e2814d74c188
Mark Wolfe <mark@wolfe.id.au>
2013-09-09 07:06:01 +1000

    Fixed issue with queue configuration.

--- test/queue-stream.js
+++ test/queue-stream.js
@@ -10,32 +10,34 @@ var queueStream = require('../lib/queue-stream.js');
 
 var expect = chai.expect;
 
+var params = {"x-message-ttl": 30000, "x-expires": 1800000, "durable": true, "autoDelete": false};
+
 describe('QueueStream', function () {
 
   it('should create a new queue stream', function (done) {
 
     var connection =
-      amqp.createConnection({url: "amqp://guest:guest@localhost:5672"})
+      amqp.createConnection({url: "amqp://guest:guest@localhost:5672"});
 
     connection.on('ready', function () {
-      log('Connection', 'open')
+      log('Connection', 'open');
 
-      queueStream({connection: connection, exchangeName: '/test/events', queueName: '/queue/events'}, function (err, stream) {
-        expect(err).to.not.exist
-        expect(stream).to.exist
-        stream.end()
-        done()
-      })
-    })
+      queueStream({connection: connection, exchangeName: '/test/events', queueName: '/queue/events/234', params: params}, function (err, stream) {
+        expect(err).to.not.exist;
+        expect(stream).to.exist;
+        stream.end();
+        done();
+      });
+    });
 
-  })
+  });
 
   it('should read data from the queue stream', function (done) {
 
     function sendData(connection, text) {
-      connection.exchange('/test/events/2', {type: 'topic'}, function (ex) {
+      connection.exchange('/test/events/2', {}, function (ex) {
         log('Exchange', ex.name, 'open')
-        ex.publish('TEST', JSON.stringify({text: text}), {contentEncoding: 'utf8', contentType: 'application/json'});
+        ex.publish('TEST', JSON.stringify({text: text, timestamp: new Date()}), {contentEncoding: 'utf8', contentType: 'application/json'});
       })
     }
 
@@ -45,18 +47,19 @@ describe('QueueStream', function () {
     connection.on('ready', function () {
       log('Connection', 'open')
 
-      queueStream({connection: connection, exchangeName: '/test/events/2', queueName: '/queue/events'}, function (err, stream) {
+      queueStream({connection: connection, exchangeName: '/test/events/2', queueName: '/queue/events/123', params: params}, function (err, stream) {
         expect(err).to.not.exist
         expect(stream).to.exist
 
-        stream.bindRoutingKey('#', function(){
+        stream.bindRoutingKey('TEST', function(){
 
           stream.pipe(through(function onData(message){
             log('onData', message)
             expect(message.text).is.equal('Hello Test');
-            stream.end()
-            done()
-          }))
+            stream.end();
+            done();
+          }));
+
 
           sendData(connection, 'Hello Test');
         })
